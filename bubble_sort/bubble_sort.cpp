4180f0ffab5d3fd66fe63c8f357fd8cc8a85df04
iamanggi <anggipuspita452@gmail.com>
2023-03-16 15:50:31 +0700

    prosedur mengurutkan array

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -2,27 +2,38 @@
 using namespace std;
 
 
-int a[20];
-int n;
+int a[20];			// deklarasi array a dengan ukuran 20 
+int n;				// deklarasi variabel n untuk menyimpan banyaknya elemen pada array 
 
-void input() {
-	while (true) {
-		cout << "masukan banyaknya elemen pada array : ";
-		cin >> n;
-		if (n <= 20)
-			break;
-		else {
-			cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
+void input() {		// prosedur untuk input 
+	while (true) {	// looping 
+		cout << "masukan banyaknya elemen pada array : ";	// output ke layar
+		cin >> n;		// input dari pengguna 
+		if (n <= 20)	// jika n kurang dari atau sama dengan 20
+			break;		// keluar dari loop
+		else {			// jika n lebih dari 20
+			cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";		// output ke layar
 		}
 	}
-	cout << endl;
-	cout << "===============" << endl;
-	cout << "masukan elemen Array" << endl;
-	cout << "===============" << endl;
+	cout << endl;							// output baris kosong 
+	cout << "===============" << endl;		// output ke layar 
+	cout << "masukan elemen Array" << endl;	// output ke layar 
+	cout << "===============" << endl;		// output ke layar 
 
-	for (int i = 0; i < n; i++) {
-		cout << "data ke- " << (i + 1) << ":";
-		cin >> a[i];
+	for (int i = 0; i < n; i++) {		// looping dengan i dimulai dari 0 hingga n-1
+		cout << "data ke- " << (i + 1) << ":";	// output ke layar 
+		cin >> a[i];		// input dari pengguna
 	}
 }
-void BubbleSortArray 
+void BubbleSortArray() {		// prosedur untuk mengurutkan array dengan metode bubble sort
+	for (int i = 1; i < n; i++) {		// looping dengan i dimulai dari 1 hingga n-1
+		for (int j = 1; j < n - 1; j++) {	// looping dengan j dimulai dari 0 hingga n-1
+			if (a[j] > a[j + 1]) {			// jika nilai pada a[j] lebih besar dari a [j + 1]
+				int temp = a[j];			// simpan nilai a[j] ke variabel sementara temp
+				a[j] = a[j + 1];			// assign nilai a[j+1] ke a[j]
+				a[j + 1] = temp;			// assign nilai temp ke a[j+1]
+			}
+		}
+	}
+
+}
