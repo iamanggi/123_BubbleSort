903468ddc63e530581f5010ce905ab5166cc293a
iamanggi <anggipuspita452@gmail.com>
2023-03-16 15:30:57 +0700

    prosedur input

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -1,20 +1,28 @@
-// bubble_sort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+int a[20];
+int n;
+
+void input() {
+	while (true) {
+		cout << "masukan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
+		}
+	}
+	cout << endl;
+	cout << "===============" << endl;
+	cout << "masukan elemen Array" << endl;
+	cout << "===============" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "data ke- " << (i + 1) << ":";
+		cin >> a[i];
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void BubbleSortArray 
